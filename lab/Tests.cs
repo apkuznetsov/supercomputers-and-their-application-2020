e5cedd88b7af4f8d9fd51e46c6cf421fe81dfd82
Andrey <kouznts@ya.ru>
2020-04-22 19:24:04 +0400

    TestTransposeMethod(int[] testArrSizes)

--- lab/Tests.cs
+++ lab/Tests.cs
@@ -25,5 +25,25 @@ namespace lab
 
             return resultsInSecs;
         }
+
+        public static double[] TestTransposeMethod(int[] testArrSizes)
+        {
+            double[] resultsInSecs = new double[testArrSizes.Length];
+
+            for (int index = 0; index < testArrSizes.Length; index++)
+            {
+                int[,] arr1 = Arrays.CreateRandNumsArr(testArrSizes[index], testArrSizes[index]);
+                int[,] arr2 = Arrays.Transpose(Arrays.CreateRandNumsArr(testArrSizes[index], testArrSizes[index]));
+
+                Stopwatch stopwatchForClassic = new Stopwatch();
+                stopwatchForClassic.Start();
+                Arrays.MultByClassicMethod(arr1, arr2);
+                stopwatchForClassic.Stop();
+
+                resultsInSecs[index] = stopwatchForClassic.Elapsed.TotalSeconds;
+            }
+
+            return resultsInSecs;
+        }
     }
 }
