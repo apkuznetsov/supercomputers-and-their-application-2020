29ed2886bd92adc428d776ad647a10a85778e67c
Andrey <kouznts@ya.ru>
2020-04-22 19:22:10 +0400

    Transpose(int[,] arr)

--- lab/Arrays.cs
+++ lab/Arrays.cs
@@ -36,5 +36,19 @@ namespace lab
 
             return resArr;
         }
+
+        public static int[,] Transpose(int[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int cols = arr.GetLength(1);
+
+            int[,] newArr = new int[cols, rows];
+
+            for (int y = 0; y < rows; y++)
+                for (int x = 0; x < cols; x++)
+                    newArr[y, x] = arr[x, y];
+
+            return newArr;
+        }
     }
 }
