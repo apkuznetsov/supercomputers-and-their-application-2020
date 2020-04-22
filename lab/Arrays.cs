af9f2b9195c38afcd736c371edcce470f4b49679
Andrey <kouznts@ya.ru>
2020-04-22 18:19:22 +0400

    MultByClassicMethod(...)

--- lab/Arrays.cs
+++ lab/Arrays.cs
@@ -11,14 +11,30 @@ namespace lab
             Random rand = new Random();
 
             for (int y = 0; y < rowsNum; y++)
-            {
                 for (int x = 0; x < colsNum; x++)
-                {
                     arr[y, x] = rand.Next();
-                }
-            }
 
             return arr;
+        }
+
+        public static int[,] MultByClassicMethod(int[,] arr1, int[,] arr2)
+        {
+            int rowsNumArr1 = arr1.GetLength(0);
+            int colsNumArr1 = arr1.GetLength(1);
+            int colsNumArr2 = arr2.GetLength(1);
+
+            int[,] resArr = new int[rowsNumArr1, colsNumArr2];
+
+            for (int rows1 = 0; rows1 < rowsNumArr1; rows1++)
+                for (int cols2 = 0; cols2 < colsNumArr2; cols2++)
+                {
+                    resArr[rows1, cols2] = 0;
+
+                    for (int cols1 = 0; cols1 < colsNumArr1; cols1++)
+                        resArr[rows1, cols2] += arr1[rows1, cols1] * arr2[cols1, cols2];
+                }
+
+            return resArr;
         }
     }
 }
