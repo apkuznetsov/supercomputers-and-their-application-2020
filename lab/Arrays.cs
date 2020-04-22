7aaac328dc7c944e2fc06d81480f80c8788264e8
Andrey <kouznts@ya.ru>
2020-04-22 18:05:17 +0400

    CreateRandNumsArr(...)

---
+++ lab/Arrays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab
+{
+    public class Arrays
+    {
+        public static int[,] CreateRandNumsArr(int rowsNum, int colsNum)
+        {
+            int[,] arr = new int[rowsNum, colsNum];
+
+            Random rand = new Random();
+
+            for (int y = 0; y < rowsNum; y++)
+            {
+                for (int x = 0; x < colsNum; x++)
+                {
+                    arr[y, x] = rand.Next();
+                }
+            }
+
+            return arr;
+        }
+    }
+}
