663e485da4a6c14933dac59fee5e54a68a173cc4
Andrey <kouznts@ya.ru>
2020-04-22 18:38:24 +0400

    TestClassicMethod(...)

---
+++ lab/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace lab
+{
+    public class Tests
+    {
+        public static readonly int[] TestArrSizes = { 32, 64, 128, 256, 512, 1024 };
+
+        public static double[] TestClassicMethod(int[] testArrSizes)
+        {
+            double[] resultsInSecs = new double[testArrSizes.Length];
+
+            for (int index = 0; index < testArrSizes.Length; index++)
+            {
+                int[,] arr1 = Arrays.CreateRandNumsArr(testArrSizes[index], testArrSizes[index]);
+                int[,] arr2 = Arrays.CreateRandNumsArr(testArrSizes[index], testArrSizes[index]);
+
+                Stopwatch stopwatchForClassic = new Stopwatch();
+                stopwatchForClassic.Start();
+                Arrays.MultByClassicMethod(arr1, arr2);
+                stopwatchForClassic.Stop();
+
+                resultsInSecs[index] = stopwatchForClassic.Elapsed.TotalSeconds;
+            }
+
+            return resultsInSecs;
+        }
+    }
+}
