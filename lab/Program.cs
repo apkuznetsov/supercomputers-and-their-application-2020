ff0816b680b43146caf7b300105666f9ac65968f
Andrey <kouznts@ya.ru>
2020-04-23 11:00:11 +0400

    TestingBlockMethod()

--- lab/Program.cs
+++ lab/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace lab
 {
@@ -6,9 +6,11 @@ namespace lab
     {
         static void Main(string[] args)
         {
-            TestingClassicMethod();
-            TestingTransposeMethod();
-        }
+            //TestingClassicMethod();
+            //TestingTransposeMethod();
+            TestingBlockMethod();
+
+            Console.ReadKey();
         }
 
         private static void TestingClassicMethod()
@@ -33,6 +35,17 @@ namespace lab
                     "длительность расчёта = " + resultsInSecs[index] + " сек.\n");
         }
 
+        private static void TestingBlockMethod()
+        {
+            const int blockSize = 1024;
+
+            Console.WriteLine("БЛОЧНОЕ ПЕРЕМНОЖЕНИЕ ДЛЯ БЛОКА РАЗМЕРОМ " + blockSize + ":\n");
+
+            double[] resultsInSecs = Tests.TestBlockMethod(blockSize, Tests.TestBlockSizes);
+            for (int index = 0; index < Tests.TestArrSizes.Length; index++)
+                Console.WriteLine("измерение " + index + "\n" +
+                    "размер матрицы = " + Tests.TestArrSizes[index] + "\n" +
+                    "длительность расчёта = " + resultsInSecs[index] + " сек.\n");
         }
     }
 }
