3f545f312b5a05229511025014ea35a0013e7b36
Andrey <kouznts@ya.ru>
2020-04-23 10:49:07 +0400

    MultByBlockMethod(...)

--- lab/BlockArrays.cs
+++ lab/BlockArrays.cs
@@ -1,7 +1,32 @@
-namespace lab
+﻿namespace lab
 {
     public class BlockArrays
     {
+        public static int[,] MultByBlockMethod(int[,] a, int[,] b, int blockSize, int currBlockSize, int startRowA, int startColA, int startRowB, int startColB)
+        {
+            if (blockSize == currBlockSize)
+                return MultByClassicMethod(a, b, currBlockSize, startRowA, startColA, startRowB, startColB);
+
+            int newBlockSize = currBlockSize / 2;
+            int[,] c11 = SumBlockSizeSquareArrs(
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA, startColA, startRowB, startColB),
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA, startColA + newBlockSize, startRowB + newBlockSize, startColB));
+
+            int[,] c12 = SumBlockSizeSquareArrs(
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA, startColA, startRowB, startColB + newBlockSize),
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA, startColA + newBlockSize, startRowB + newBlockSize, startColB + newBlockSize));
+
+            int[,] c21 = SumBlockSizeSquareArrs(
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA + newBlockSize, startColA, startRowB, startColB),
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA + newBlockSize, startColA + newBlockSize, startRowB + newBlockSize, startColB));
+
+            int[,] c22 = SumBlockSizeSquareArrs(
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA + newBlockSize, startColA, startRowB, startColB + newBlockSize),
+                MultByBlockMethod(a, b, blockSize, newBlockSize, startRowA + newBlockSize, startColA + newBlockSize, startRowB + newBlockSize, startColB + newBlockSize));
+
+            return JoinBlockSizeSquareArr(c11, c12, c21, c22);
+        }
+
         private static int[,] MultByClassicMethod(int[,] a, int[,] b, int blockSize, int startRowA, int startColA, int startRowB, int startColB)
         {
             int[,] c = new int[blockSize, blockSize];
@@ -18,14 +43,17 @@ namespace lab
             return c;
         }
 
-        private static int[,] SumSquareArrs(int[,] a, int[,] b)
+        private static int[,] SumBlockSizeSquareArrs(int[,] a, int[,] b)
         {
             int blockSize = a.GetLength(0);
             int[,] c = new int[blockSize, blockSize];
-        
+
             for (int y = 0; y < blockSize; y++)
                 for (int x = 0; x < blockSize; x++)
                     c[y, x] = a[y, x] + b[y, x];
+
+            return c;
+        }
 
         private static int[,] JoinBlockSizeSquareArr(int[,] c11, int[,] c12, int[,] c21, int[,] c22)
         {
@@ -51,6 +79,6 @@ namespace lab
                     }
 
             return c;
+        }
     }
-}
 }
