83215f9092f706ebff5ec85b39e0cac92f78d0e1
Andrey <kouznts@ya.ru>
2020-04-23 18:16:02 +0400

    TestDoubleBlockMethod(...)

--- lab/Tests/BlockTests.cs
+++ lab/Tests/BlockTests.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace lab.Tests
 {
@@ -17,6 +17,28 @@ namespace lab.Tests
                 stopwatch.Start();
 
                 IntBlockArrays.MultByBlockMethod(arr1, arr2, testBlockSizes[index], arr1.GetLength(0), 0, 0, 0, 0);
+
+                stopwatch.Stop();
+
+                resultsInSecs[index] = stopwatch.Elapsed.TotalSeconds;
+            }
+
+            return resultsInSecs;
+        }
+
+        public static double[] TestDoubleBlockMethod(int testArrSize, int[] testBlockSizes)
+        {
+            double[] resultsInSecs = new double[testBlockSizes.Length];
+
+            for (int index = 0; index < testBlockSizes.Length; index++)
+            {
+                double[,] arr1 = Arrays.CreateRandNumsIntArr(testArrSize, testArrSize);
+                double[,] arr2 = Arrays.CreateRandNumsIntArr(testArrSize, testArrSize);
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                DoubleBlockArrays.MultByBlockMethod(arr1, arr2, testBlockSizes[index], arr1.GetLength(0), 0, 0, 0, 0);
 
                 stopwatch.Stop();
 
