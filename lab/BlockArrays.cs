b6aa5276bb3a6e9dfd4606e5bade7b22b587b44d
Andrey <kouznts@ya.ru>
2020-04-23 10:25:19 +0400

    MultByClassicMethod(...)

---
+++ lab/BlockArrays.cs
@@ -0,0 +1,23 @@
+﻿namespace lab
+{
+    public class BlockArrays
+    {
+        private static int[,] MultByClassicMethod(int[,] a, int[,] b, int blockSize, int startRowA, int startColA, int startRowB, int startColB)
+        {
+            int[,] c = new int[blockSize, blockSize];
+
+            for (int y = 0; y < blockSize; y++)
+                for (int x = 0; x < blockSize; x++)
+                {
+                    c[y, x] = 0;
+
+                    for (int k = 0; k < blockSize; k++)
+                        c[y, x] += a[y + startRowA, k + startColA] * b[k + startRowB, x + startColB];
+                }
+
+            return c;
+        }
+
+        
+    }
+}
