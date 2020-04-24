df78a6a4d39e769c79d244c446a5102b4bf9312f
Andrey <kouznts@ya.ru>
2020-04-24 17:12:42 +0400

    переименовать test1

--- lab2/test1.cpp
+++ lab2/test1.cpp
@@ -1,6 +1,3 @@
-// lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
@@ -49,14 +46,3 @@ int main()
 	double time = (time2 - time1) / MY_PROCESSOR_CLOCKS_PER_SEC;
 	cout << "time = " << time << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
