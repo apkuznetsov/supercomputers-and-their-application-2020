6c8dac16458bcb5cce48cf28fe6390b9de86a7f7
Andrey <kouznts@ya.ru>
2020-04-24 22:32:59 +0400

    test_dynamic_double()

--- lab2/dynamic_creation.cpp
+++ lab2/dynamic_creation.cpp
@@ -40,6 +40,8 @@ int test_dynamic_int() {
 	delete[] a;
 	delete[] b;
 	delete[] c;
+
+	return 0;
 }
 
 int test_dynamic_float() {
@@ -58,6 +60,40 @@ int test_dynamic_float() {
 
 	for (int i = 0; i < ARR_SIZE; i++)
 		b[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+	time1 = __rdtsc();
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		c[i] = a[i] + b[i];
+
+	time2 = __rdtsc();
+
+	double time = (time2 - time1) / MY_PROCESSOR_CLOCKS_PER_SEC;
+	cout << time << endl;
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+
+	return 0;
+}
+
+int test_dynamic_double() {
+	const int ARR_SIZE = 40000;
+
+	const double MY_PROCESSOR_CLOCKS_PER_SEC = 1800000000;
+	unsigned __int64 time1 = 0;
+	unsigned __int64 time2 = 0;
+
+	double* a = new double[ARR_SIZE];
+	double* b = new double[ARR_SIZE];
+	double* c = new double[ARR_SIZE];
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		a[i] = rand() / RAND_MAX;
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		b[i] = rand() / RAND_MAX;
 
 	time1 = __rdtsc();
 
