2b0c4873402afb30dd1b46b23086869f2b5afaa6
Andrey <kouznts@ya.ru>
2020-04-24 21:52:17 +0400

    test_int()

--- lab2/static_creation.cpp
+++ lab2/static_creation.cpp
@@ -10,8 +10,38 @@
 #define _USE_MATH_DEFINES
 using namespace std;
 
+int test_int() {
+	const int ARR_SIZE = 40000;
+
+	const double MY_PROCESSOR_CLOCKS_PER_SEC = 1800000000;
+	unsigned __int64 time1 = 0;
+	unsigned __int64 time2 = 0;
+
+	int a[ARR_SIZE], b[ARR_SIZE], c[ARR_SIZE];
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		a[i] = rand();
+
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		b[i] = rand();
+
+
+	time1 = __rdtsc();
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		c[i] = a[i] + b[i];
+
+	time2 = __rdtsc();
+
+	double time = (time2 - time1) / MY_PROCESSOR_CLOCKS_PER_SEC;
+	cout << time << endl;
+
+	return 0;
+}
+
 int test_float() {
-	const int ARR_SIZE = 85000;
+	const int ARR_SIZE = 40000;
 
 	const double MY_PROCESSOR_CLOCKS_PER_SEC = 1800000000;
 	unsigned __int64 time1 = 0;
