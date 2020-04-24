04dea5c6c2249cc478f6c9f4fae60bcee28e7f38
Andrey <kouznts@ya.ru>
2020-04-24 22:30:21 +0400

    test_dynamic_int()

--- lab2/dynamic_creation.cpp
+++ lab2/dynamic_creation.cpp
@@ -10,6 +10,38 @@
 #define _USE_MATH_DEFINES
 using namespace std;
 
+int test_dynamic_int() {
+	const int ARR_SIZE = 40000;
+
+	const double MY_PROCESSOR_CLOCKS_PER_SEC = 1800000000;
+	unsigned __int64 time1 = 0;
+	unsigned __int64 time2 = 0;
+
+	int* a = new int[ARR_SIZE];
+	int* b = new int[ARR_SIZE];
+	int* c = new int[ARR_SIZE];
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		a[i] = rand();
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		b[i] = rand();
+
+	time1 = __rdtsc();
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		c[i] = a[i] + b[i];
+
+	time2 = __rdtsc();
+
+	double time = (time2 - time1) / MY_PROCESSOR_CLOCKS_PER_SEC;
+	cout << time << endl;
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+}
+
 int test_dynamic_float() {
 	const int ARR_SIZE = 40000;
 
